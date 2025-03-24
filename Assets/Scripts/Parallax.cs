de15df6683ec9486f4bf50fc805c868b82c174fa
Jhosus-ui <jhosu.uchiha@gmail.com>
2025-03-23 22:15:41 -0400

    Gaming

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -4,25 +4,32 @@ using System.Collections.Generic;
 
 public class Parallax : MonoBehaviour
 {
-
     [SerializeField] private Vector2 velocidadMovimiento;
 
     private Vector2 offset;
     private Material material;
-
     private Rigidbody2D jugadorRB;
+    private Vector2 velocidadAnterior;
 
     private void Awake()
     {
-       material = GetComponent<SpriteRenderer>().material;
-       jugadorRB = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
-
+        material = GetComponent<SpriteRenderer>().material;
+        jugadorRB = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+        velocidadAnterior = jugadorRB.linearVelocity;
     }
 
     private void Update()
     {
-        offset = (jugadorRB.linearVelocity.x * 0.1f) * velocidadMovimiento * Time.deltaTime;
-        material.mainTextureOffset += offset;
-    }
+        Vector2 velocidadActual = jugadorRB.linearVelocity;
 
+        // Solo aplica el efecto parallax si el jugador se est� moviendo
+        if (velocidadActual != velocidadAnterior)
+        {
+            offset = (jugadorRB.linearVelocity.x * 0.1f) * velocidadMovimiento * Time.deltaTime;
+            material.mainTextureOffset += offset;
+        }
+
+        // Actualiza la velocidad anterior
+        velocidadAnterior = velocidadActual;
+    }
 }
